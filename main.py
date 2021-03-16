587b45c6b9f38d0b9a1fa79633440039bdb333be
Ducvoccer <ntduc250798@gmail.com>
2021-03-16 07:25:32 +0700

    a

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-import random
-import time
-r = str(random.randint(0,1000))
+# -*- coding: utf-8 -*-
+from fbchat.models import *
+from fbchat import log, Client
+import json
 
-with open('a_'+r, 'w') as f:
-    f.write('hihi')
-time.sleep(200)
+# list_emails = ['phimtonghop37@gmail.com',
+#                'duc.nt161113@sis.hust.edu.vn', 'voccer.it@gmail.com']
+email = 'voccer.it@gmail.com'
+
+password = ''
+# user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"
+
+# Load session đăng nhập từ trước nếu co
+session_cookies = ''
+try:
+    with open('session/session_gen_{}.json'.format(email)) as f:
+        session_cookies = json.load(f)
+except Exception as e:
+    print(e)
+
+client = Client(email, password, user_agent=user_agent,
+                session_cookies=session_cookies)
+client.send(Message(text='hi'),
+            thread_id='100009594708355',
+            thread_type=ThreadType.USER
+            )
