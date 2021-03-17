4ef571c696cffadbe8262247360a51e3ed0944e5
Ducvoccer <ntduc250798@gmail.com>
2021-03-17 08:59:36 +0700

    change branch

--- main.py
+++ main.py
@@ -21,8 +21,13 @@ except Exception as e:
 
 client = Client(email, password, user_agent=user_agent,
                 session_cookies=session_cookies)
+
+import datetime
+
 while True:
-    client.send(Message(text='hello'),
+    now = datetime.datetime.now()
+    text = str(now.day) + '.'+ str(now.hour) +':'+ str(now.minute)
+    client.send(Message(text=text),
             thread_id='100009594708355',
             thread_type=ThreadType.USER
             )
--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-class Test():
-    __a = 0
-    _b=1
-    def __init__(self):
-        self.__a = 5
-        # print(self.__a)
-    def __get_a(self):
-        print('comehere')
-        return __a
-test = Test()
-# test.__get_a()
-print(test._b)
-print(test.__a)
-# print(test.__a)
+import datetime
+now = datetime.datetime.now()
+print(now.hour)
+print(now.minute)
+print(now.day)
